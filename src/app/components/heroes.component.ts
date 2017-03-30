46553e646b235dc8229e0ee9cafe55a23de7d9e9
KOCMOC <KOCMOC@192.168.2.16>
2017-03-30 15:40:12 +0200

    WORKING WITH STEP 7 / HTTP / CHECKPOINT D

--- src/app/components/heroes.component.ts
+++ src/app/components/heroes.component.ts
@@ -35,6 +35,15 @@ export class HeroesComponent implements OnInit {
             });
     }
 
+    delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) {this.selectedHero = null;}
+            });
+    }
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }

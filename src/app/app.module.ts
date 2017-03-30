5e71bf95dd07bd638c9877a30a83a01db392701b
KOCMOC <KOCMOC@192.168.2.16>
2017-03-30 11:53:22 +0200

    WORKING WITH STEP 6 / ROUTING / CHECKPOINT F

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
-import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './components/app.component';
 import {HeroDetailComponent} from './components/hero-detail.component';
@@ -9,31 +8,14 @@ import {HeroesComponent} from './components/heroes.component';
 import {DashboardComponent} from './components/dashboard.component';
 import {HeroService} from './services/hero.service'
 
+import {AppRoutingModule} from './app-routing.module';
 
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            },
-        ]),
+        AppRoutingModule
     ],
     declarations: [ // this one is needed to recognize the "tags" <my-app></my-app>,<my-heroes></>, etc.  
         AppComponent,

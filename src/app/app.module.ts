d3edb9dc8bdf7843505e3805f330a514cf715b91
KOCMOC <KOCMOC@192.168.2.16>
2017-03-30 15:07:47 +0200

    WORKING WITH STEP 7 / HTTP / CHECKPOINT A

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
+
+import {AppRoutingModule} from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import {AppComponent} from './components/app.component';
 import {HeroDetailComponent} from './components/hero-detail.component';
@@ -8,14 +15,16 @@ import {HeroesComponent} from './components/heroes.component';
 import {DashboardComponent} from './components/dashboard.component';
 import {HeroService} from './services/hero.service'
 
-import {AppRoutingModule} from './app-routing.module';
+
 
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
-        AppRoutingModule
+        AppRoutingModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
     ],
     declarations: [ // this one is needed to recognize the "tags" <my-app></my-app>,<my-heroes></>, etc.  
         AppComponent,
--- src/app/services/hero.service.js
+++ src/app/services/hero.service.js
@@ -9,27 +9,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var mock_heroes_1 = require('./mock-heroes');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/toPromise');
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
+        this.heroesUrl = 'api/heroes'; // URL to web api
     }
+    HeroService.prototype.getHeroes = function () {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    };
     HeroService.prototype.getHero = function (id) {
         return this.getHeroes().then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
     };
-    HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
-    };
-    //Simulate a Server request, replace getHeroes() in "app.component.ts"
-    HeroService.prototype.getHeroesSlowly = function () {
-        var _this = this;
-        return new Promise(function (resolve) {
-            // Simulate server latency with 2 second delay
-            setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
-        });
-    };
     HeroService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], HeroService);
     return HeroService;
 }());

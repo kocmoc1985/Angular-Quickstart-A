45939d08a0ce801176e941d089cd2f2eeb7afab5
KOCMOC <KOCMOC@192.168.2.16>
2017-03-30 11:08:18 +0200

    WORKING WITH STEP 6 / ROUTING / CHECKPOINT D

--- src/app/components/dashboard.component.ts
+++ src/app/components/dashboard.component.ts
@@ -1,7 +1,21 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../classes/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
-    selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+  selector: 'my-dashboard',
+  templateUrl: 'app/templates/dashboard.component.html',
 })
-export class DashboardComponent {}
+
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
